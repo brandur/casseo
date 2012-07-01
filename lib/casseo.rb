2a670503c17ae7fc93577b909addce30c6cd6e58
Brandur <brandur@mutelight.org>
2012-06-30 19:20:54 -0700

    Using require_relative for Gem installation

--- lib/casseo.rb
+++ lib/casseo.rb
@@ -1,4 +1,4 @@
-require "./lib/casseo/config"
-require "./lib/casseo/index"
-require "./lib/casseo/dashboard"
-require "./lib/casseo/version"
+require_relative "casseo/config"
+require_relative "casseo/index"
+require_relative "casseo/dashboard"
+require_relative "casseo/version"
